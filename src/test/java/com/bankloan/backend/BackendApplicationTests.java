fd8e3a97a3f5d57eb0b4dadcfda3027ba36ebefb
akash <akashkumarcs23@gmail.com>
2026-05-22 00:17:43 +0530

    Initial backend setup

---
+++ src/test/java/com/bankloan/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankloan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
